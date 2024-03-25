007c7388ecb94844a07feec9bedad96b432de57d
Тормышев Николай <n.tormyshev@getqa.ru>
2024-03-25 11:02:56 +0300

    дофига всякого написали

--- kvur.py
+++ kvur.py
@@ -1,1 +1,15 @@
 print("Вас привет система решения квадратных уравнений")
+bad_data = True
+while bad_data :
+    try:
+        a = int(input("Введите a:"))
+        b = int(input("Введите b:"))
+        c = int(input("Введите c:"))
+        bad_data=False
+    except ValueError:
+        print("Не пиши фигню.")
+    except ZeroDivisionError:
+        print("Убери нули")
+    except:
+        print("Чет фигня какая-то")
+print(a + b)
