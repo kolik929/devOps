416cb684b1c3b4054e05bb3b3de3eabcd88ec170
Тормышев Николай <n.tormyshev@getqa.ru>
2024-03-27 09:21:36 +0300

    some change

--- client.py
+++ client.py
@@ -4,8 +4,7 @@ import time
 while True:
     time.sleep(5)
     result = requests.get("http://myserver/time")
-    #print(result.text)
-    with open('/srv/log.txt','a') as logfile :
+    # print(result.text)
+    with open('/srv/log.txt', 'a') as logfile:
         logfile.write(result.text)
         logfile.write("\n")
-
